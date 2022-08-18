41d17814602f406343746c01341b98b702615d73
KcPele <fidekg123@gmail.com>
2022-08-18 22:10:59 +0100

    the last but not the list

--- static/js/general.js
+++ static/js/general.js
@@ -3,12 +3,14 @@
 $(document).ready(function(){
   function setDark() {
     $("input, textarea, .head-section,  .upload-container,.result-container,.getting-in, .right-side, .profile-search").css("background-color", "#303233")
-    $("body, .first, .container, .start-btn, .popup, .spinner__background, .history-data, .faq, .section-container, .main-flow-flex").css("background-color", "#1f1f1f")
+    $("body, .first, .container, .start-btn, .popup, .loader-wrapper, .loader, .loader-wrapper, .loader-section, .history-data, .faq, .section-container, .main-flow-flex").css("background-color", "#1f1f1f")
     $(".container, .popup, input").css("color", "white")
+   
   }
   function unSetDark () {
     $("input, textarea, .head-section, .upload-container, .result-container, .getting-in, .right-side, .profile-search").css("background-color", "#fafafa")
-    $("body, .first, .container, .popup, .start-btn, .spinner__background, .history-data, .faq, .section-container, .main-flow-flex").css("background-color", "white")
+    $("body, .first, .container, .popup, .start-btn, .loader, .loader-wrapper, .loader-section, .history-data, .faq, .section-container, .main-flow-flex").css("background-color", "white")
+    $("input[type=submit]").css("background-color", "rgba(86, 204, 242, 1)")
   }
   let isDark = JSON.parse(localStorage.getItem("isDark"))
   $('#toggleTheme').prop("checked", isDark)
@@ -28,21 +30,12 @@ $(document).ready(function(){
       localStorage.setItem("isDark", JSON.stringify(isChecked))
   });
 
+  $("input[type=submit]").click(function() {
+      $(".spinner").css("display", "block")
+      console.log("click a button")
+  })
 
 });
-
-const submitBtn = document.getElementsByTagName("input[type=submit]");
-
-submitBtn.addEventListener("click", function () {
-  console.log("click");
-  document.getElementsByClassName("spinner__background").style.display =
-    "block";
-});
-console.log('working testingsd')
-
-setTimeout(function () {
-  document.getElementsByClassName("spinner__background").style.display = "none";
-}, 6000);
 
 
 

3482b1f4002e08015a38712be7095fb8cdeb4948
KcPele <fidekg123@gmail.com>
2022-08-19 12:17:48 +0100

    contact done

--- static/js/general.js
+++ static/js/general.js
@@ -2,13 +2,14 @@
 
 $(document).ready(function(){
   function setDark() {
-    $("input, textarea, .head-section,  .upload-container,.result-container,.getting-in, .right-side, .profile-search").css("background-color", "#303233")
-    $("body, .first, .container, .start-btn, .popup, .loader-wrapper, .loader, .loader-wrapper, .loader-section, .history-data, .faq, .section-container, .main-flow-flex").css("background-color", "#1f1f1f")
+    $("input, textarea, .head-section, .field-container, .nav-aside, .forms-wrapper,  .upload-container,.result-container,.getting-in, .right-side, .profile-search").css("background-color", "#303233")
+    $("body, .first, .container,input[type=submit], .start-btn, .popup, .loader-wrapper, .loader, .loader-wrapper, .loader-section, .history-data, .faq, .section-container, .main-flow-flex").css("background-color", "#1f1f1f")
     $(".container, .popup, input").css("color", "white")
-   
+    $("input, span, label, textarea, .head-section, .nav-aside, .forms-wrapper,  .upload-container,.result-container,.getting-in, .right-side, .profile-search").css("color", "rgb(155 152 152)")
+    $(".dropdown img").css("background", "white")
   }
   function unSetDark () {
-    $("input, textarea, .head-section, .upload-container, .result-container, .getting-in, .right-side, .profile-search").css("background-color", "#fafafa")
+    $("input, textarea, .head-section, .field-container, .nav-aside, .forms-wrapper, .upload-container, .result-container, .getting-in, .right-side, .profile-search").css("background-color", "#fafafa")
     $("body, .first, .container, .popup, .start-btn, .loader, .loader-wrapper, .loader-section, .history-data, .faq, .section-container, .main-flow-flex").css("background-color", "white")
     $("input[type=submit]").css("background-color", "rgba(86, 204, 242, 1)")
   }
